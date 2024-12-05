d88b4b906c42e11d6637c8f3b7216b825bf69328
Chxmxs <aboufuss@gmail.com>
2024-12-04 21:30:45 -0500

    Add full property list

--- charneskymonopolyfinal.py
+++ charneskymonopolyfinal.py
@@ -35,3 +35,29 @@ class Property:
             rent_amount = self.rent 
             player.cash -= rent_amount 
             self.owner.cash += rent_amount 
+ 
+#USED UMGPT for full property list 
+properties = [ 
+    Property("Mediterranean Avenue", 60, 2), 
+    Property("Baltic Avenue", 60, 4), 
+    Property("Oriental Avenue", 100, 6), 
+    Property("Vermont Avenue", 100, 6), 
+    Property("Connecticut Avenue", 120, 8), 
+    Property("St. Charles Place", 140, 10), 
+    Property("States Avenue", 140, 10), 
+    Property("Virginia Avenue", 160, 12), 
+    Property("St. James Place", 180, 14), 
+    Property("Tennessee Avenue", 180, 14), 
+    Property("New York Avenue", 200, 16), 
+    Property("Kentucky Avenue", 220, 18), 
+    Property("Indiana Avenue", 220, 18), 
+    Property("Illinois Avenue", 240, 20), 
+    Property("Atlantic Avenue", 260, 22), 
+    Property("Ventnor Avenue", 260, 22), 
+    Property("Marvin Gardens", 280, 24), 
+    Property("Pacific Avenue", 300, 26), 
+    Property("North Carolina Avenue", 300, 26), 
+    Property("Pennsylvania Avenue", 320, 28), 
+    Property("Park Place", 350, 35), 
+    Property("Boardwalk", 400, 50) 
+] 
